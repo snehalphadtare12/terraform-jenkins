89a0861a4aeda93a51450ab31cd86e0b662747ed
snehalphadtare12 <spsnehalphadtare06@gmail.com>
2026-02-02 21:10:38 +0530

    Add variables for AWS configuration

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
